3dd8e64a58649d3510155c7d5439c65c7a138776
climu <climmu@gmail.com>
2016-06-02 06:38:43 -0400

    add archive to look for a specific user games by req"

--- asrleague/archive-iframe.php
+++ asrleague/archive-iframe.php
@@ -17,15 +17,17 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/asrleague/bootstrap.php';
 // messages
 $req = array_merge($_GET, $_POST);
 $msg = '';
+$user='';
 
 echo bootstrap_head("Advanced Study Room -- Games Archive");
 if (isset($req['msg'])) { $msg = substr($req['msg'], 0, 256); }
+if (isset($req['user'])) { $user = $req['user']; }
 ?>
 
 
 <body>
         <div class="">
-<?php if ( is_user_logged_in() && current_user_can('asr_edit_events') ) { ?>
+<?php if ( is_user_logged_in() && current_user_can('asr_edit_events')&& (strlen($user)==0 )) { ?>
         <h2>Add Game Records</h2>
           <form action="action.php" method="post" enctype="multipart/form-data"><table><tr><td><input type="hidden" name="action" value="upload_sgf"><input type="file" id="upload_sgf" name="upload_sgf"></td><td><button type="submit" role="button" class="btn btn-sm btn-success">Send</button></td></tr></table></form>
           </form>
@@ -33,8 +35,14 @@ if (isset($req['msg'])) { $msg = substr($req['msg'], 0, 256); }
 <?php } ?>
         <h2>Records Archive</h2>
         <?php
+	if (strlen($user)>0){
+          $games = DB::query("SELECT id,pstatus,data_key,player_white,player_black,date,place,type,result FROM sgf WHERE player_white=%s OR player_black=%s ORDER BY date LIMIT 10000;",$user,$user);
+}
+else{
+
           $games = DB::query("SELECT id,pstatus,data_key,player_white,player_black,date,place,type,result FROM sgf ORDER BY date LIMIT 10000;");
-          $t = '<div class="table-responsive">'."\n";
+   }
+	  $t = '<div class="table-responsive">'."\n";
           $t .= '<table class="table" id="table-players">'."\n";
           $t .= '<thead><tr><th>Date</th><th>White</th><th>Black</th><th>Result</th></tr></thead>'."\n";
           $t .= '<tbody>'."\n";
